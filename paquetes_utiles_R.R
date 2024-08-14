aab748804c8cffa986af60bed8be7a41c3941010
Esteban Cardoso <ecardoso@ine.gub.uy>
2024-08-14 14:22:59 -0300

    primer commit

---
+++ paquetes_utiles_R.R
@@ -0,0 +1,113 @@
+# instalacion de paquetes generica
+
+install.packages("tidyverse")
+
+install.packages('stringi')
+
+# instalaci?n de paquetes explicitando el repositorio
+
+install.packages("tidyverse", dependencies = TRUE, repos =  'http://cran.rstudio.com/')
+
+
+# instalaci?n de varios paquetes
+
+install.packages (c("srvyr", "markdown", "readxl"))
+
+install.packages("readODS")
+
+install.packages("pander",  dependencies = TRUE, repos =  'http://cran.rstudio.com/')
+
+
+install.packages(kableExtra, dependencies = TRUE, repos = 'http://cran.rstudio.com/')
+
+install.packages("janitor", dependencies = TRUE, repos = 'http://cran.rstudio.com/')
+
+install.packages("tabularray", dependencies = TRUE, repos = 'http://cran.rstudio.com/')
+
+install.packages("readr", dependencies= TRUE, repos= 'http://cran.rstudio.com/' )
+
+# Chequeo de los registros que aparecen m?s de una vez en ASSE (me trae solo un registro en caso duplicado y dos registros en caso triplicado).
+
+
+registros_asse <- subset(asse_07_total,duplicated(documento))
+
+
+# Chequeo de los registros que aparecen m?s de una vez en BPS (me trae solo un registro en caso duplicado y dos registros en caso triplicado).
+
+
+registros_bps <- subset(asse_bps_07,duplicated(documento))
+
+
+install.packages( 'DataExplorer' , dependencies = TRUE , repos='http://cran.rstudio.com/' )
+
+install.packages( 'naniar' , dependencies = TRUE , repos='http://cran.rstudio.com/' )
+
+install.packages( 'sqldf' , dependencies = TRUE , repos='http://cran.rstudio.com/' )
+
+install.packages("misty", dependencies= TRUE, repos= 'http://cran.rstudio.com/' )
+
+# paquete para ver las tablas interactivas
+
+install.packages("DT", dependencies=TRUE, repos='http://cran.rstudio.com/')
+library(DT)
+
+
+# Paquete para abrir archivos xml
+
+library(xml2)
+
+install.packages("xml2", dependencies=TRUE, repos= 'http://cran.rstudio.com/')
+
+install.packages("plotly", dependencies= TRUE, repos= 'http://cran.rstudio.com/' )
+
+# Paquete para escribir en código SQL en R
+
+install.packages("sqldf", dependencies= TRUE, repos= 'http://cran.rstudio.com/' )
+
+install.packages("dbplyr", dependencies= TRUE, repos= 'http://cran.rstudio.com/' )
+
+# Paquete de control de calidad 
+
+install.packages("dlookr", dependencies= TRUE, repos= 'http://cran.rstudio.com/' ) 
+
+library(dbplyr, warn.conflicts = FALSE)
+
+# Paquete necesario para automatizar código
+
+install.packages("taskscheduleR")
+
+library(taskscheduleR)
+
+
+# 
+
+install.packages("rowr", dependencies = TRUE, repos = 'http://cran.rstudio.com/')
+
+library(rowr)
+
+
+# Paquete necesario para leer archivos parquet 
+
+install.packages("arrow", dependencies= TRUE, repos= 'http://cran.rstudio.com/' )
+
+# Paquete para manejar grandes volumenes de datos
+
+install.packages("disk.frame")
+
+# Paquete par citar
+
+install.packages("bibtex",  dependencies= TRUE, repos= 'http://cran.rstudio.com/' )
+
+
+# janitor para limpiar nombres de las columnas
+
+install.packages("janitor",  dependencies= TRUE, repos= 'http://cran.rstudio.com/' )
+
+# PhantomJS es una herramienta que se utiliza para capturar imágenes de páginas web, y
+# webshot lo utiliza internamente para convertir HTML a imágenes, que luego pueden ser incluidas en el PDF.
+
+webshot::install_phantomjs()
+
+# Mactheo probabilístico
+
+install.packages("fastLink")
